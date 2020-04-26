05934b0d94c7a8d3c14c12fffa06c904e0356080
Amen Jlili <amen@bluebyte.biz>
2020-04-25 21:42:00 -0700

    finished proof concept and added implementation to core project

--- PDMStandardBatchTool.Console/EntryPoint.cs
+++ PDMStandardBatchTool.Console/EntryPoint.cs
@@ -4,8 +4,11 @@ using System.ComponentModel.Design;
 using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Automation;
+using System.Windows.Forms;
+using CADSharpTools.SOLIDWORKS;
 using ConisioSW2Lib;
 using Microsoft.Win32;
 using Microsoft.WindowsAPICodePack.Shell;
@@ -19,35 +22,142 @@ namespace PDMStandardBatchTool.Console
 
         public static void Main()
         {
+            string checkInComment = "This is a check-in comment";
             var pdmAddInClsId = Helper.GetPDMAddInGuid();
-            var windowOpenedEventHandler = new AutomationEventHandler((object sender, AutomationEventArgs e) => {
+            string fileName = @"C:\PDM2019\PDM2019\30-151127.sldprt";
+            string addinDllFilename = @"C:\Program Files\SOLIDWORKS Corp\SOLIDWORKS PDM\pdmsw.dll";
+        
+            var checkoutDialogOpened = new AutomationEventHandler((object sender, AutomationEventArgs e) => {
 
                 var window = sender as AutomationElement;
+
+                var windowTitle = window.Current.Name;
+
+                if (windowTitle != "SOLIDWORKS PDM")
+                    return;
+
+                bool catchPhraseFound = false; 
+                string catchPhrase = string.Empty;
+                catchPhrase = "Would you like to check it out?";
+
                 var control = TreeWalker.ControlViewWalker.GetFirstChild(window);
                 while (control != null)
                 {
+                    var text = control.Current.Name;
+                    if (string.IsNullOrWhiteSpace(text) == false)
+                    {
+                        if (text.Contains(catchPhrase))
+                            catchPhraseFound = true; 
+                    }
                     // to do some stuff here
-
-
-
-
                     control = TreeWalker.ControlViewWalker.GetNextSibling(control);
+
+                    
                 }
 
+
+                if (catchPhraseFound) 
+                { 
+                control = TreeWalker.ControlViewWalker.GetFirstChild(window);
+                while (control != null)
+                {
+                    if (control.Current.ControlType == ControlType.Button)
+                        {
+                            var text = control.Current.Name;
+                            if (string.IsNullOrWhiteSpace(text) == false)
+                            {
+                                if (text.Contains("Yes"))
+                                {
+                                    var invokePattern = control.GetCurrentPattern(InvokePattern.Pattern) as InvokePattern;
+
+                                    if (invokePattern != null)
+                                        invokePattern.Invoke();
+                                }
+
+                            }
+
+                        }
+                   
+                    // to do some stuff here
+                    control = TreeWalker.ControlViewWalker.GetNextSibling(control);
+
+
+                }
+
+                }
+
+
+                Automation.RemoveAllEventHandlers();
+            });
+            var checkInDialogOpened = new AutomationEventHandler((object sender, AutomationEventArgs e) => {
+
+                var window = sender as AutomationElement;
+
+                var windowTitle = window.Current.Name;
+
+                if (windowTitle != "Check in")
+                    return;
+
+               
+                var control = TreeWalker.ControlViewWalker.GetFirstChild(window);
+             
+                while (control != null)
+                {
+                    if (control.Current.ClassName == "Edit")
+                    {
+
+                        control.SetFocus();
+                        Thread.Sleep(100);
+                        SendKeys.SendWait(checkInComment);
+                         
+
+                    }
+
+                    // to do some stuff here
+                    control = TreeWalker.ControlViewWalker.GetNextSibling(control);
+
+
+                }
+
+
+                control = TreeWalker.ControlViewWalker.GetFirstChild(window);
+                    while (control != null)
+                    {
+                        if (control.Current.ControlType == ControlType.Button)
+                        {
+                            var text = control.Current.Name;
+                            if (string.IsNullOrWhiteSpace(text) == false)
+                            {
+                                if (text.Contains("Check in"))
+                                {
+                                    var invokePattern = control.GetCurrentPattern(InvokePattern.Pattern) as InvokePattern;
+
+                                    if (invokePattern != null)
+                                        invokePattern.Invoke();
+                                }
+
+                            }
+
+                        }
+
+                        // to do some stuff here
+                        control = TreeWalker.ControlViewWalker.GetNextSibling(control);
+
+
+                    }
+
+                
+
+
+                Automation.RemoveAllEventHandlers();
             });
 
-
-            string addinDllFilename = string.Empty;
             ConisioSW2Lib.IConisioSWAddIn addinObject = default(ConisioSW2Lib.IConisioSWAddIn);
 
-            using (var app = new CADSharpTools.SOLIDWORKS.Application(-1, true))
-            {
-                var swApp = app.SOLIDWORKS;
-
-                addinDllFilename = @"C:\Program Files\SOLIDWORKS Corp\SOLIDWORKS PDM\pdmsw.dll";
-
+               var swApp = CADSharpTools.SOLIDWORKS.Application.CreateSldWorks(false, 30);
+             
+                 
                 var loadRet = (swLoadAddinError_e)swApp.LoadAddIn(addinDllFilename);
-
                 switch (loadRet)
                 {
                     case swLoadAddinError_e.swUnknownError:
@@ -69,7 +179,31 @@ namespace PDMStandardBatchTool.Console
                             var sldworksElement = AutomationElement.FromHandle(windowHandle);
                             if (sldworksElement != null)
                             {
-                                Automation.AddAutomationEventHandler(WindowPattern.WindowOpenedEvent, sldworksElement, TreeScope.Subtree, windowOpenedEventHandler);
+                                Automation.AddAutomationEventHandler(WindowPattern.WindowOpenedEvent, sldworksElement, TreeScope.Children, checkoutDialogOpened);
+
+                            var docType = swDocumentTypes_e.swDocPART;
+                            var extension = System.IO.Path.GetExtension(fileName).ToLower();
+                            if (extension.Contains("sldprt"))
+                                docType = swDocumentTypes_e.swDocPART;
+                            else if (extension.Contains("sldasm"))
+                                docType = swDocumentTypes_e.swDocASSEMBLY;
+                            else if (extension.Contains("slddrw"))
+                                docType = swDocumentTypes_e.swDocDRAWING;
+
+                            var modelDoc = swApp.OpenDoc(fileName, (int)docType) as ModelDoc2;
+
+
+                            //  this is where we would execute a vba macro 
+                            modelDoc.Save();
+                            Automation.AddAutomationEventHandler(WindowPattern.WindowOpenedEvent, sldworksElement, TreeScope.Children, checkInDialogOpened);
+                            // check in document 
+                            addinObject.Unlock();
+
+                            // wait until document is readonly
+                            
+                            // close document
+                            swApp.CloseAllDocuments(true);
+                            System.Console.ReadLine();
                             }
                         
                         }
@@ -98,7 +232,7 @@ namespace PDMStandardBatchTool.Console
                 
 
                 swApp.ExitApp();
-            }
+                System.Runtime.InteropServices.Marshal.ReleaseComObject(swApp);
             
 
             System.Console.ReadLine();
--- PDMStandardBatchTool.Core/Core.cs
+++ PDMStandardBatchTool.Core/Core.cs
@@ -1,8 +1,218 @@
 ﻿
 
+using SolidWorks.Interop.sldworks;
+using SolidWorks.Interop.swconst;
+using System;
+using System.Threading;
+using System.Windows.Automation;
+using System.Windows.Forms;
+
 namespace PDMStandardBatchTool.Core
 {
+
     public class Core
     {
+        public static MacroParameters MacroParameters;
+        public static SldWorks Application { get; set; }
+
+        public static ConisioSW2Lib.IConisioSWAddIn addinObject { get; private set; }
+
+        public static bool ProcessFile(string filename, string checkInComment)
+        {
+            var ret = false;
+
+            var checkoutDialogOpened = new AutomationEventHandler((object sender, AutomationEventArgs e) => {
+                var window = sender as AutomationElement;
+
+                var windowTitle = window.Current.Name;
+
+                if (windowTitle != "SOLIDWORKS PDM")
+                    return;
+
+                bool catchPhraseFound = false;
+                string catchPhrase = string.Empty;
+                catchPhrase = "Would you like to check it out?";
+
+                var control = TreeWalker.ControlViewWalker.GetFirstChild(window);
+                while (control != null)
+                {
+                    var text = control.Current.Name;
+                    if (string.IsNullOrWhiteSpace(text) == false)
+                    {
+                        if (text.Contains(catchPhrase))
+                            catchPhraseFound = true;
+                    }
+                    // to do some stuff here
+                    control = TreeWalker.ControlViewWalker.GetNextSibling(control);
+
+
+                }
+
+
+                if (catchPhraseFound)
+                {
+                    control = TreeWalker.ControlViewWalker.GetFirstChild(window);
+                    while (control != null)
+                    {
+                        if (control.Current.ControlType == ControlType.Button)
+                        {
+                            var text = control.Current.Name;
+                            if (string.IsNullOrWhiteSpace(text) == false)
+                            {
+                                if (text.Contains("Yes"))
+                                {
+                                    var invokePattern = control.GetCurrentPattern(InvokePattern.Pattern) as InvokePattern;
+
+                                    if (invokePattern != null)
+                                        invokePattern.Invoke();
+                                }
+
+                            }
+
+                        }
+
+                        // to do some stuff here
+                        control = TreeWalker.ControlViewWalker.GetNextSibling(control);
+
+
+                    }
+
+                }
+
+
+                Automation.RemoveAllEventHandlers();
+            });
+            var checkInDialogOpened = new AutomationEventHandler((object sender, AutomationEventArgs e) => {
+
+                var window = sender as AutomationElement;
+
+                var windowTitle = window.Current.Name;
+
+                if (windowTitle != "Check in")
+                    return;
+
+
+                var control = TreeWalker.ControlViewWalker.GetFirstChild(window);
+
+                while (control != null)
+                {
+                    if (control.Current.ClassName == "Edit")
+                    {
+
+                        control.SetFocus();
+                        Thread.Sleep(100);
+                        SendKeys.SendWait(checkInComment);
+
+
+                    }
+
+                    // to do some stuff here
+                    control = TreeWalker.ControlViewWalker.GetNextSibling(control);
+
+
+                }
+
+
+                control = TreeWalker.ControlViewWalker.GetFirstChild(window);
+                while (control != null)
+                {
+                    if (control.Current.ControlType == ControlType.Button)
+                    {
+                        var text = control.Current.Name;
+                        if (string.IsNullOrWhiteSpace(text) == false)
+                        {
+                            if (text.Contains("Check in"))
+                            {
+                                var invokePattern = control.GetCurrentPattern(InvokePattern.Pattern) as InvokePattern;
+
+                                if (invokePattern != null)
+                                    invokePattern.Invoke();
+                            }
+
+                        }
+
+                    }
+
+                    // to do some stuff here
+                    control = TreeWalker.ControlViewWalker.GetNextSibling(control);
+
+
+                }
+
+
+
+
+                Automation.RemoveAllEventHandlers();
+            });
+             
+
+
+                // add ui automation event handler 
+                var frame = Application.Frame() as Frame;
+                var windowHandle = new IntPtr(frame.GetHWndx64());
+                var sldworksElement = AutomationElement.FromHandle(windowHandle);
+            if (sldworksElement != null)
+            {
+                Automation.AddAutomationEventHandler(WindowPattern.WindowOpenedEvent, sldworksElement, TreeScope.Children, checkoutDialogOpened);
+
+                var docType = swDocumentTypes_e.swDocPART;
+                var extension = System.IO.Path.GetExtension(fileName).ToLower();
+                if (extension.Contains("sldprt"))
+                    docType = swDocumentTypes_e.swDocPART;
+                else if (extension.Contains("sldasm"))
+                    docType = swDocumentTypes_e.swDocASSEMBLY;
+                else if (extension.Contains("slddrw"))
+                    docType = swDocumentTypes_e.swDocDRAWING;
+
+                var modelDoc = swApp.OpenDoc(fileName, (int)docType) as ModelDoc2;
+
+
+                //  this is where we would execute a vba macro 
+                modelDoc.Save();
+                Automation.AddAutomationEventHandler(WindowPattern.WindowOpenedEvent, sldworksElement, TreeScope.Children, checkInDialogOpened);
+                // check in document 
+                addinObject.Unlock();
+
+                // wait until document is readonly
+
+                // close document
+                swApp.CloseAllDocuments(true);
+                System.Console.ReadLine();
+            }
+          
+
+
+
+
+            return ret;
+
+        }
+
+        public static bool LoadPDMAddIn(string dllPath)
+        {
+            var ret = false;
+
+            var loadRet = (swLoadAddinError_e)Application.LoadAddIn(dllPath);
+
+            if (loadRet == swLoadAddinError_e.swAddinAlreadyLoaded || loadRet == swLoadAddinError_e.swSuccess)
+                ret = true;
+
+
+                string pdmAddInClsId = Helper.GetPDMAddInGuid();
+                addinObject = Application.GetAddInObject(pdmAddInClsId) as ConisioSW2Lib.IConisioSWAddIn;
+            if (addinObject == null)
+                throw new Exception("Failed to load PDM add-in");
+
+            return ret;
+        }
+
+    }
+
+    public struct MacroParameters
+    {
+        public string FilePathName;
+        public string ModuleName;
+        public string ProcedureName; 
+
     }
 }
